b1064ed0f84c927ffc5f905e3ada9348f3b9b299
pavancos <pavankc005@gmail.com>
2024-12-27 22:36:17 +0530

    feat: filling Myspace and Craftbench Cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,33 +12,8 @@ export default {
       fontFamily: {
         afacad: ['"Afacad"', 'sans-serif'],
       },
-      animation: {
-        typewriter: 'typewriter 2s steps(11) forwards',
-        caret: 'typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s',
-      },
-      keyframes: {
-        typewriter: {
-          to: {
-            left: '100%',
-          },
-        },
-        blink: {
-          '0%': {
-            opacity: '0',
-          },
-          '0.1%': {
-            opacity: '1',
-          },
-          '50%': {
-            opacity: '1',
-          },
-          '50.1%': {
-            opacity: '0',
-          },
-          '100%': {
-            opacity: '0',
-          },
-        },
+      cursor: {
+        paint: 'url(./assets/paintCursor.cur),pointer',
       },
     }
   },
